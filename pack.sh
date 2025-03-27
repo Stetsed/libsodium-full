b56f75c392caee94671ddad07fef6e53f4e85e92
Stetsed <stetsed@selfhostable.net>
2025-03-27 09:17:31 +0100

    Fixing and pinned

--- pack.sh
+++ pack.sh
@@ -4,13 +4,13 @@
 
 set -euxo pipefail
 
-# Reset submodule state
+# # Reset submodule state
 git -C libsodium reset --hard HEAD
 
 git submodule update --init
 
 for f in patches/*; do
-    git -C libsodium apply "../${f}"
+	git -C libsodium apply "../${f}"
 done
 
 pio package pack -o dist/
